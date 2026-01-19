f507d7079ee12093f1fdc80c3f2ef7aa1181636f
mridul1101 <84969910+mridul1101@users.noreply.github.com>
2026-01-19 20:29:37 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # Constants
-API_KEY = "AIzaSyDd02LbjboeF8AeRX46oTW8Z9gc1Yx6YCk"
+API_KEY = "AIzaSyCPFn4GA_6HAAe4BkFGXGfHUJmGs5xjFKQ"
 MODEL_NAME = "gemini-2.0-flash"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+
 INDEX_PATH = "my_faiss_index"
